5aef97a688fca2ad38014e61cfebef2e3b2a8504
Bad Hacker <113774126+hasibu409@users.noreply.github.com>
2022-10-07 16:47:32 +0600

    Update fb.py

--- fb.py
+++ fb.py
@@ -46,7 +46,7 @@ return False
 
 
 if __name__ == "__main__":
-print('\n---------- Welcome To Facebook BruteForce ----------\n')
+print('\n---------- Welcome To BAD  ----------\n')
 if not os.path.isfile(PASSWORD_FILE):
 print("Password file is not exist: ", PASSWORD_FILE)
 sys.exit(0)
